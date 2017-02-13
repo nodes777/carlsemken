e8cac2486ecf59549a5898b36d28795a20fe4a59
nodes777 <nodes777@gmail.com>
2017-02-13 11:23:30 -0800

    fixes just images being sent out

--- bot.js
+++ bot.js
@@ -3,14 +3,11 @@
     var json = JSON.stringify(response,null,2);
     fs.writeFile("pokemonExample.json",json);
 */
-
 // Dependencies =========================
 var
     twit = require('twit'),
     config = require('./config'),
-    quotes = require('./quotes'),
     Pokedex = require('pokedex-promise-v2'),
-    request = require('request'),
     fs = require('fs');
 
 var P = new Pokedex();
@@ -24,118 +21,136 @@ var stream = Twitter.stream('user');
 
 
 //POKEMON ======================================
+tweetRandomPokemon(getRandomPokeNum(maxPokeRange));
 
-getPokemonPromise(getRandomPokeNum(maxPokeRange));
-
-
-
-function getPokemonPromise(num){
+function tweetRandomPokemon(num) {
 
     P.getPokemonSpeciesByName(num) // with Promise
         .then(function(response) {
-        var obj = {};
-        console.log("getting poke name and flavor text..");
-        obj.name = response.name;
-        //get the flavor text, remove the newlines in it
-        obj.flavorText = response.flavor_text_entries[1].flavor_text.replace(/\r?\n|\r/g, " ");
-        obj.num = num;
-        console.log(obj.num);
-        return obj;
-    })
-    .catch(function(error) {
+            var obj = {};
+            console.log("getting poke name and flavor text..");
+            obj.name = response.name;
+            //get the flavor text, remove the newlines in it
+            obj.flavorText = response.flavor_text_entries[1].flavor_text.replace(/\r?\n|\r/g, " ");
+            obj.num = num;
+            console.log(obj.num);
+            return obj;
+        })
+        .catch(function(error) {
             console.log('There was an error getting by species name ');
-         })
+        })
 
-    .then(addImage)
-    .then(buildTweet)
-    .then(uploadImage)
-    .then(sendTweet)
-    .catch(function(error) {
-      console.log('There was an ERROR: ', error);
-    });
+        .then(addImage).catch(function(error) {
+            console.log('There was an error adding the image ');
+        })
+        .then(buildTweet).catch(function(error) {
+            console.log('There was an error building the tweet ');
+        })
+        .then(uploadImage).catch(function(error) {
+            console.log('There was an error uploading the image ');
+        })
+        .then(sendTweet).catch(function(error) {
+            console.log('There was an error sending the tweet ');
+        })
+        .catch(function(error) {
+            console.log('There was an ERROR: ', error);
+        });
 }
 
-function addImage(pokeObj){
+function addImage(pokeObj) {
     console.log('Adding image...');
     pokeObj.img = pokeObj.num;
     return pokeObj;
 }
 
-function buildTweet(pokeObj){
+function buildTweet(pokeObj) {
     console.log("building tweet...");
     pokeObj.name = pokeObj.name.charAt(0).toUpperCase() + pokeObj.name.slice(1);
-    var tweet = "#"+pokeObj.name + "\n"+ pokeObj.flavorText;
+    var tweet = "#" + pokeObj.name + "\n" + pokeObj.flavorText;
 
-        if(tweet.length>140){
+    if (tweet.length > 140) {
 
-            var numberOfTweets =  Math.ceil(tweet.length/135);
-            var tweetParts = [];
-            var start = 0;
-            var distance = 135;
+        var numberOfTweets = Math.ceil(tweet.length / 135);
+        var tweetParts = [];
+        var start = 0;
 
-            //console.log("Number of tweets needed: "+numberOfTweets+"\n");
-            for(var i = 0; i<numberOfTweets; i++){
-                tweetPart =  getTweetPart(tweet, start);
-                start += tweetPart.length;
+        for (var i = 0; i < numberOfTweets; i++) {
+            var tweetPart = getTweetPart(tweet, start);
+            start += tweetPart.length;
 
-                //add in the tweet number
-                tweetPart += "("+(i+1)+"/"+numberOfTweets+")";
+            //add in the tweet number
+            tweetPart += "(" + (i + 1) + "/" + numberOfTweets + ")";
 
-                tweetParts.push(tweetPart);
-            }
-            pokeObj.tweetParts = tweetParts;
-            return pokeObj;
-
-        } else {
-            pokeObj.tweetParts = [tweet];
-            return pokeObj;
+            tweetParts.push(tweetPart);
         }
+        pokeObj.tweetParts = tweetParts;
+        return pokeObj;
+
+    } else {
+        pokeObj.tweetParts = [tweet];
+        return pokeObj;
+    }
 }
 
 
-function uploadImage(pokeObj){
+function uploadImage(pokeObj) {
     console.log("uploading image... ");
-    var filePath = "./img/pokemon"+pokeObj.img+".png";
+    var filePath = "./img/pokemon" + pokeObj.img + ".png";
     var params = {
         encoding: 'base64'
     };
 
     var b64 = fs.readFileSync(filePath, params);
-    var promise = new Promise(function(resolve, reject){Twitter.post("media/upload", {media_data: b64}, function (err, data, response) {
-            pokeObj.imgId =  data.media_id_string;
-            console.log("after media_data "+pokeObj);
+    var promise = new Promise(function(resolve, reject) {
+        Twitter.post("media/upload", {
+            media_data: b64
+        }, function(err, data, response) {
+            pokeObj.imgId = data.media_id_string;
             resolve(pokeObj);
         });
-    })
+    });
     return promise;
 }
 
 
 
-function sendTweet(pokeObj){
+function sendTweet(pokeObj) {
     console.log("sending tweet...");
-    for(var i = 0; i<=pokeObj.tweetParts.length; i++){
+    for (var i = 0; i <= pokeObj.tweetParts.length; i++) {
         var tweet = {
-        status: pokeObj.tweetParts[i],
-        media_ids: [pokeObj.imgId]
-    };
-        Twitter.post('statuses/update', tweet, postCallBack);
+            status: pokeObj.tweetParts[i],
+            media_ids: [pokeObj.imgId]
+        };
+
+    //Crumby hack to prevent just images going out, h as in "http://imagepath.png"
+        if(tweet.status[0] !== "h"){
+            console.log("Tweet: "+tweet.status);
+            Twitter.post('statuses/update', tweet, tweeted);
+        }
     }
 
+}
+
+function tweeted(error, tweet, response) {
+    if (error) {
+        console.log("Error trying to tweet: " + error);
+    } else {
+        console.log("Successful Tweet! " + tweet.text); // Tweet text.
+    }
 }
 // REPLY BOT ==========================
 
 //anytime someone tweets
 stream.on('tweet', tweetEvent);
 
-function tweetEvent(eventMsg){
+function tweetEvent(eventMsg) {
     console.log("listening...");
     var replyTo = eventMsg.in_reply_to_screen_name;
     var text = eventMsg.text;
     var from = eventMsg.user.screen_name;
 
-    if(replyTo == 'carl_semken'){
-        var newTweet = "@"+from+' thanks for tweeting me';
+    if (replyTo == 'carl_semken') {
+        var newTweet = "@" + from + ' thanks for tweeting me';
         sendTweet([newTweet]);
     }
 }
@@ -146,10 +161,10 @@ function tweetEvent(eventMsg){
 stream.on('follow', followed);
 
 function followed(eventMsg) {
-  console.log("Follow event!");
-  var name = eventMsg.source.name;
-  var screenName = eventMsg.source.screen_name;
-  sendTweet(['.@' + screenName + " I'm a robot"]);
+    console.log("Follow event!");
+    var name = eventMsg.source.name;
+    var screenName = eventMsg.source.screen_name;
+    sendTweet(['.@' + screenName + " I'm a robot"]);
 }
 
 // Fav BOT ==========================
@@ -164,7 +179,7 @@ var favoriteTweet = function() {
     Twitter.get('search/tweets', params, function(err, data) {
         var tweet = data.statuses;
         //pick a random tweet
-        var randomTweet = ranDom(tweet); //ranDom is defined below
+        var randomTweet = randomizer(tweet); //ranDom is defined below
 
         if (typeof randomTweet != 'undefined') {
             Twitter.post('favoites/create', {
@@ -181,40 +196,31 @@ var favoriteTweet = function() {
 };
 
 
-
 // favorite a tweet in every hour, just offset it by 15 mins
 //setTimeOut(function(){setInterval(favoriteTweet, 3600000);}(360000/4));
 //setInterval(favoriteTweet, 3600000);
 //Tweet a pokemon every 6 hours
-setInterval(function(){getPokemonPromise(getRandomPokeNum(maxPokeRange));}, 3600000*6);
+setInterval(function() {
+    tweetRandomPokemon(getRandomPokeNum(maxPokeRange));
+}, 3600000 * 6);
 
 
 //Utils ================================
 
-
-
-function postCallBack (error, tweet, response){
-          if(error){
-            console.log("Error trying to tweet: "+ error);
-          } else {
-                console.log("Successful Tweet! " + tweet.text);  // Tweet text.
-            }
-        }
-
-function ranDom(arr) {
+function randomizer(arr) {
     var index = Math.floor(Math.random() * arr.length);
     return arr[index];
 }
 
 
-function getTweetPart(str, beg){
+function getTweetPart(str, beg) {
     var index = 0;
 
     str = str.substr(beg, 135);
     //if the str part is less than 135 characters, return it
-        if(str.length<135){
-            return str;
-        }
+    if (str.length < 135) {
+        return str;
+    }
     //find the last space and break there
     index = str.lastIndexOf(' ');
     str = str.substr(0, index);
@@ -222,7 +228,7 @@ function getTweetPart(str, beg){
     return str;
 }
 
-function getRandomPokeNum(max){
+function getRandomPokeNum(max) {
     var randomNum = 0;
     randomNum = Math.floor(Math.random() * max);
     return randomNum;
